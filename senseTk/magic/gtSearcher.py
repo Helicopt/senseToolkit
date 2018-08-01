ce8fcf3bc5669e47fc272f673fe15932e2270702
fengweitao <fengweitao@sensetime.com>
2018-08-01 20:50:12 +0800

    fix gtSearch

--- senseTk/magic/gtSearcher.py
+++ senseTk/magic/gtSearcher.py
@@ -136,7 +136,7 @@ class gtSearcher(object):
 					item = self.fdict[two]
 					if splMatch(one.split('!!')[-1], two.split('!!')[-1]) and ''.join(one.split('!!')[:-1])==''.join(two.split('!!')[:-1]):
 						self.fdict[one] = (v[0], v[1], v[2]+item[2])
-						v = self.fdict[one]
+						v = list(set(self.fdict[one]))
 						del unMerge[i]
 						continue
 					i += 1
