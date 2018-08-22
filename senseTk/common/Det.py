df3e2caa85068c77a96511051341ab95763b7ed9
fengweitao <fengweitao@sensetime.com>
2018-08-23 01:50:19 +0800

    fix typo

--- senseTk/common/Det.py
+++ senseTk/common/Det.py
@@ -151,7 +151,7 @@ class Det(object):
     def _trim(self, sz = None, toInt = True):
         if sz is not None:
             w, h = sz
-            x1, y1, x2, y2 = self.x1, self.x2, self.y1, self.y2
+            x1, y1, x2, y2 = self.x1, self.y1, self.x2, self.y2
             x1 = max(x1, 0)
             y1 = max(y1, 0)
             x2 = min(w, x2)
