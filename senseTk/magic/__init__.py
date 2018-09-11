8ee4d8b8aaea79a6b28943cedfa6f1b869a19ecd
fengweitao <fengweitao@sensetime.com>
2018-09-11 19:47:57 +0800

    update README

--- senseTk/magic/__init__.py
+++ senseTk/magic/__init__.py
@@ -6,7 +6,10 @@
 # mail: fengweitao@sensetime.com
 # Created Time: 2018年07月27日 星期五 12时31分26秒
 #########################################################################
-from ImageGallery import IMGallery, requireQA
+try:
+    from ImageGallery import IMGallery, requireQA
+except:
+    pass
 from FileAgent import *
 from GridSearch import gridSearcher, OrderedDict
 from gtSearcher import gtSearcher, default_gt_search_config
