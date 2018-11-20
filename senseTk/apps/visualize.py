533e69a9df6b14e0e7845773ca8947ef1f28d247
fengweitao <fengweitao@sensetime.com>
2018-11-20 21:56:46 +0800

    smooth visualize

--- senseTk/apps/visualize.py
+++ senseTk/apps/visualize.py
@@ -18,6 +18,7 @@ if __name__=='__main__':
     parser.add_argument('--ifmt', default='', type=str, help = 'decide input imgset format')
     parser.add_argument('--istart', default=1, type=int, help = 'input imgset start offset')
     parser.add_argument('--trackset', default='', type=str, help = 'file recording tracklets')
+    parser.add_argument('--format', '-f', default='MOT', type=str, choices = ['MOT', 'MOTwithScore'], help = 'file recording tracklets')
     args = parser.parse_args()
     if args.ifmt=='':
         a = VideoClipReader(args.src, start = args.istart)
@@ -25,7 +26,10 @@ if __name__=='__main__':
         a = VideoClipReader(args.src, fmt = args.ifmt, start = args.istart)
     requireQA()
     if args.trackset!='':
-        g = TrackSet(args.trackset)
+        if args.format=='MOTwithScore':
+            g = TrackSet(args.trackset, formatter = 'fr.i id.i x1 y1 w h cf st -1 -1')
+        else:
+            g = TrackSet(args.trackset)
         def cb(im, ind, **kwargs):
             if kwargs['type']==IMGallery.E_REFRESH:
                 txt = ''
