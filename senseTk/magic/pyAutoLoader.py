7428da8505e0044a7874f5c160881539f4336366
fengweitao <fengweitao@sensetime.com>
2019-03-04 23:13:13 +0800

    fix

--- senseTk/magic/pyAutoLoader.py
+++ senseTk/magic/pyAutoLoader.py
@@ -1,16 +1,43 @@
 import os
+import sys
 
 def initLoader(cur, global_dict, class_name = '__auto__', disable_module = True):
     root = os.path.dirname(os.path.abspath(cur))
-    if cur[0]=='/':
-        cur = cur[len(os.getcwd())+1:]
-    if cur[-11:]=='__init__.py' or cur[-12:]=='__init__.pyc':
+    pkg_path = root
+    sysPaths = [os.path.abspath(i) for i in sys.path]
+    found = False
+    while len(pkg_path):
+        for i in sysPaths:
+            if pkg_path==i:
+                cur = os.path.relpath(cur, pkg_path)
+                found = True
+                break
+        if found: break
+        parent = os.path.dirname(pkg_path)
+        if parent==pkg_path: break
+        pkg_path = parent
+    if not found:
+        pkg_path = root
+        while len(pkg_path):
+            f = os.path.join(pkg_path, '__init__.py')
+            if not os.path.exists(f):
+                cur = os.path.relpath(cur, pkg_path)
+                found = True
+            if found: break
+            parent = os.path.dirname(pkg_path)
+            if parent==pkg_path: break
+            pkg_path = parent
+
+    # if cur[0]=='/':
+    #     cur = cur[len(os.getcwd())+1:]
+    if cur[-11:]=='__init__.py' or cur[-12:] in ['__init__.pyc', '__init__.pyo', '__init__.pyw']:
         path = os.path.dirname(cur).replace('/', '.')
     else:
-        if cur[-3:]=='.py':
-            path = cur[:-3].replace('/', '.')
-        elif cur[-4:]=='.pyc':
-            path = cur[:-4].replace('/', '.')
+        # if cur[-3:]=='.py':
+        #     path = cur[:-3].replace('/', '.')
+        # elif cur[-4:] in ['.pyc', '.pyo', '.pyw']:
+        #     path = cur[:-4].replace('/', '.')
+        raise Exception('Please launch initLoader in __init__.py file.')
     for f in os.listdir(root):
         mname = ''
         if os.path.isfile(os.path.join(root,f)) and f[-3:]=='.py' and f!='__init__.py':
@@ -20,7 +47,11 @@ def initLoader(cur, global_dict, class_name = '__auto__', disable_module = True)
             mname = f
         if mname!='':
             try:
-                __import__('%s.%s'%(path, mname))
+                if path!='':
+                    __import__('%s.%s'%(path, mname))
+                else:
+                    __import__('%s'%(mname))
+                print('%s.%s'%(path, mname))
             except ValueError:
                 raise Exception('module not exists: [%s.%s], package %s'%(path, mname, cur))
             if class_name == '__auto__':
