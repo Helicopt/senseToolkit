fca1278b6c0be2e007aa80ec6d78cc250c4e6422
fengweitao <fengweitao@sensetime.com>
2019-07-04 23:51:18 +0800

    simplify initLoader

--- senseTk/magic/pyAutoLoader.py
+++ senseTk/magic/pyAutoLoader.py
@@ -15,25 +15,45 @@ def initLoader(cur, module_dict, class_name = '__auto__'):
     else:
         curs = cur
     curs = list(map(os.path.abspath, curs))
-    dirs = set()
-    for c in curs:
-        d = os.path.abspath(os.path.dirname(c))
-        dirs.add(d)
 
     olds = sys.path
-    sys.path = list(dirs) + olds
+    sysPaths = [os.path.abspath(i) for i in olds]
 
+    cache = {}
     for c in curs:
         b, ext = os.path.splitext(os.path.basename(c))
-        if ext in ['.py', '.pyo', '.pyw', '.pyc'] and len(b)>0 and b[0]!='_':
-            if class_name is None:
-                m = __import__(b)
-            elif class_name=='__auto__':
-                m = getattr(__import__(b), b)
+        if ext in ['.py', '.pyo', '.pyw', '.pyc', ''] and len(b)>0 and b[0]!='_':
+            d = os.path.abspath(os.path.dirname(c))
+            r = cache.get(d, False)
+            if r==False:
+                pkg_path = d
+                r = None
+                while len(pkg_path):
+                    for i in sysPaths:
+                        if pkg_path==i:
+                            r = os.path.relpath(d, pkg_path)
+                            break
+                    if r is not None: break
+                    parent = os.path.dirname(pkg_path)
+                    if parent==pkg_path: break
+                    pkg_path = parent
+                cache[d] = r
+            if r is not None:
+                if r=='.':
+                    mname = b
+                else:
+                    mname = r.replace('/', '.') + '.' + b
             else:
-                m = getattr(__import__(b), class_name)
+                sys.path = [d] + olds
+                mname = b
+            if class_name is None:
+                m = __import__(mname, fromlist=[''])
+            elif class_name=='__auto__':
+                m = getattr(__import__(mname, fromlist=['']), b)
+            else:
+                m = getattr(__import__(mname, fromlist=['']), class_name)
             module_dict[b] = m
 
-    sys.path = olds
+            sys.path = olds
 
     return module_dict
